ae0d079d9fb13522c0dfe113155fad8abe68998b
wangxun <1013296326@qq.com>
2025-08-01 14:26:59 +0800

    up

--- viper/core/db.py
+++ viper/core/db.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
-engine = create_engine(
-    'mysql+mysqldb://user:password@host:port/dbname',
+DATABASE_URL = "mysql+asyncmy://user:password@localhost:3306/dbname"
+
+engine = create_async_engine(
+    DATABASE_URL,
+    max_overflow=20,  # 最大溢出
     pool_recycle=1800,
 )
