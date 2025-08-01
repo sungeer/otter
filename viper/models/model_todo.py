f7cd7f48d62cda6701edde060247cf254178347c
wangxun <1013296326@qq.com>
2025-08-01 14:29:27 +0800

    add

--- viper/models/model_todo.py
+++ viper/models/model_todo.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 from viper.core.db import engine
 
 
-def get_todos():
+async def get_todos():
     sql_str = '''
         SELECT 
             id, name
@@ -14,14 +14,14 @@ def get_todos():
     '''
     params = {'max_id': 10}
 
-    with engine.connect() as conn:
-        result = conn.execute(text(sql_str), params)
+    async with engine.connect() as conn:
+        result = await conn.execute(text(sql_str), params)
         # data is [] or [{'id': 1, 'name': 'a'}, {'id': 2, 'name': 'b'}]
         data = result.mappings().all()
     return data
 
 
-def get_todo_by_id():
+async def get_todo_by_id():
     sql_str = '''
         SELECT 
             id, name
@@ -33,28 +33,28 @@ def get_todo_by_id():
     '''
     params = {'id': 123}
 
-    with engine.connect() as conn:
-        result = conn.execute(text(sql_str), params)
+    async with engine.connect() as conn:
+        result = await conn.execute(text(sql_str), params)
         # data is None or {'id': 123, 'name': '张三'}
         data = result.mappings().first()
     return data
 
 
-def add_todo():
+async def add_todo():
     sql_str = '''
         INSERT INTO your_table (name, age)
         VALUES (:name, :age)
     '''
     params = {'name': '张三', 'age': 20}
 
-    with engine.begin() as conn:
-        result = conn.execute(text(sql_str), params)
+    async with engine.begin() as conn:
+        result = await conn.execute(text(sql_str), params)
         # rowcount = result.rowcount  # 插入成功，受影响行数
         inserted_id = result.lastrowid  # 自增主键id
     return inserted_id
 
 
-def add_todos():
+async def add_todos():
     sql_str = '''
         INSERT INTO your_table (name, age)
         VALUES (:name, :age)
@@ -65,26 +65,26 @@ def add_todos():
         {'name': '王五', 'age': 25},
     ]
 
-    with engine.begin() as conn:
-        result = conn.execute(text(sql_str), data)
+    async with engine.begin() as conn:
+        result = await conn.execute(text(sql_str), data)
         rowcount = result.rowcount  # 插入成功，受影响行数
     return rowcount
 
 
-def del_todo():
+async def del_todo():
     sql_str = '''
         DELETE FROM your_table
         WHERE id=:id
     '''
     params = {'id': 5}
 
-    with engine.begin() as conn:
-        result = conn.execute(text(sql_str), params)
+    async with engine.begin() as conn:
+        result = await conn.execute(text(sql_str), params)
         rowcount = result.rowcount  # 删除成功，受影响行数
     return rowcount
 
 
-def update_todo():
+async def update_todo():
     sql_str = '''
         UPDATE your_table
         SET name=:name, age=:age
@@ -92,7 +92,7 @@ def update_todo():
     '''
     params = {'name': '李四', 'age': 22, 'id': 3}
 
-    with engine.begin() as conn:
-        result = conn.execute(text(sql_str), params)
+    async with engine.begin() as conn:
+        result = await conn.execute(text(sql_str), params)
         rowcount = result.rowcount  # 更新成功，受影响行数
     return rowcount
