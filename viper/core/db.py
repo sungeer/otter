72410cd1ea43e73e3f6409a8eac6b5b676851148
wangxun <1013296326@qq.com>
2025-11-12 15:06:43 +0800

    add

--- viper/core/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-
-DATABASE_URL = "mysql+asyncmy://user:password@localhost:3306/dbname"
-
-engine = create_async_engine(
-    DATABASE_URL,
-    max_overflow=20,  # 最大溢出
-    pool_recycle=1800,
-)
--- viper/services/service_todo.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-
-async def stream_data():
-    for i in range(5):
-        await asyncio.sleep(1)
-        yield {'progress': i * 20, 'info': f'step {i}'}
-    yield {'progress': '', 'info': 'finish'}
